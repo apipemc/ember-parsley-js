1c6ffe295c950c5cb40d59ab4719ba4c84108db8
Andres Felipe Martinez Cordero <andrepipem@gmail.com>
2017-05-09 09:23:35 -0500

    Add more options and events

--- addon/components/form-parsley.js
+++ addon/components/form-parsley.js
@@ -1,44 +1,70 @@
 import Ember from 'ember';
 import layout from '../templates/components/form-parsley';
 
-export default Ember.Component.extend({
+const { Component, assign } = Ember;
+
+export default Component.extend({
   layout,
 
   tagName: 'form',
 
   parsleyRef: null,
 
-  //Attribute default options
+  // Default data-namespace for DOM API
+  namespace: 'data-parsley-',
+  // Supported inputs by default
   inputs: 'input, textarea, select',
+  // Excluded inputs by default
   excluded: 'input[type=button], input[type=submit], input[type=reset], input[type=hidden]',
+  // Stop validating field on highest priority failing constraint
   priorityEnabled: true,
+  // identifier used to group together inputs (e.g. radio buttons...)
   multiple: null,
+  // identifier (or array of identifiers) used to validate only a select group of inputs
   group: null,
+  // Enable\Disable error messages
   uiEnabled: true,
+  // Key events threshold before validation
   validationThreshold: 3,
+  // Focused field on form validation error. 'first'|'last'|'none'
   focus: 'first',
-  eventTrigger: false,
+  // event(s) that will trigger validation before first failure. eg: `input`...
+  eleTrigger: false,
+  // event(s) that will trigger validation after first failure.
   triggerAfterFailure: 'input',
+  // Class that would be added on every failing validation Parsley field
   errorClass: 'parsley-error',
+  // Same for success validation
   successClass: 'parsley-success',
+  // ul elem that would receive errors' list
+  errorsWrapper: '<ul class="parsley-errors-list"></ul>',
+  // li elem that would receive error message
+  errorTemplate: '<li></li>',
 
   didInsertElement() {
     this._super(...arguments);
 
-    let options = {
-      inputs: this.get('inputs'),
-      excluded: this.get('excluded'),
-      priorityEnabled: this.get('priorityEnabled'),
-      multiple: this.get('multiple'),
-      group: this.get('group'),
-      uiEnabled: this.get('uiEnabled'),
-      validationThreshold: this.get('validationThreshold'),
-      focus: this.get('focus'),
+    let options = this.getProperties(
+    ['inputs',
+      'excluded',
+      'priorityEnabled',
+      'multiple',
+      'group',
+      'uiEnabled',
+      'validationThreshold',
+      'focus',
+      'triggerAfterFailure',
+      'errorClass',
+      'successClass',
+      'errorsWrapper',
+      'errorTemplate'
+    ]);
+
+    assign(options, {
       trigger: this.get('eventTrigger'),
-      triggerAfterFailure: this.get('triggerAfterFailure'),
-      errorClass: this.get('errorClass'),
-      successClass: this.get('successClass')
-    };
+      classHandler: this._classHandler.bind(this),
+      errorsContainer: this._errorsContainer.bind(this),
+    });
 
     let parsleyRef = this.$().parsley(options);
 
@@ -49,9 +75,17 @@ export default Ember.Component.extend({
     this.get('parsleyRef').destroy();
   },
 
-  submit(e){
+  submit(e) {
     e.preventDefault();
     this.sendAction();
+  },
+
+  _classHandler(element) {
+    this.sendAction('classHandler', element);
+  },
+
+  _errorsContainer(element) {
+    this.sendAction('errorsContainer', element);
   }
 
 });
